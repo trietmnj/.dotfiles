f76352d860b12bef8233d6da26d203534ce9326e
TMNT <trietpersonal@gmail.com>
2021-12-01 22:44:36 -0500

    added shortcut to mupdf

---
+++ fish/.config/fish/functions/pdf.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function pdf --wraps=/mnt/c/Apps/mupdf-1.19.0-windows/mupdf.exe --description 'alias pdf=mupdf.exe'
+  /mnt/c/Apps/mupdf-1.19.0-windows/mupdf-gl.exe  $argv;
+end
