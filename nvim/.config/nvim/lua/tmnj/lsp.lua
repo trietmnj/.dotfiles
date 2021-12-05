bd4f78fc8e6c545d2a158ef5606019ec22280098
TMNT <trietpersonal@gmail.com>
2021-12-04 22:50:25 -0500

    mv treesitter config to lsp.vim

--- nvim/.config/nvim/lua/tmnj/lsp.lua
+++ nvim/.config/nvim/lua/tmnj/lsp.lua
@@ -164,3 +164,23 @@ lsp_installer.on_server_ready(function(server)
     vim.cmd [[ do User LspAttachBuffers ]]
 end)
 
+-- treesitter
+require'nvim-treesitter.configs'.setup {
+    indent = { enable = true },
+    highlight = { enable = true },
+    incremental_selection = { enable = true },
+    textobjects = { enable = true },
+    rainbow = {
+        enable = true,
+        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+        max_file_lines = nil, -- Do not enable for files with more than n lines, int
+        colors = {
+            '#97e023',
+            '#78DCE8',
+            '#dfd561',
+            '#fa8419',
+            '#9c64fe'
+            }, -- table of hex strings
+        -- termcolors = {} -- table of colour name strings
+  }}
