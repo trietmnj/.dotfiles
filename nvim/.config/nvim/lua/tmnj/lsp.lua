19a50173bf82e8b2324fb5b04b228900b3625a0e
Nguyen <rdcrltmn@crlhmb-mn-r6548.erdc.dren.mil>
2022-09-12 09:06:49 -0400

    lsp deprecation

--- nvim/.config/nvim/lua/tmnj/lsp.lua
+++ nvim/.config/nvim/lua/tmnj/lsp.lua
@@ -32,9 +32,14 @@ local on_attach = function(client, bufnr)
     buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
 
     -- Set some keybinds conditional on server capabilities
-    if client.resolved_capabilities.document_formatting then
+    -- if client.resolved_capabilities.document_formatting then
+    --     buf_set_keymap("n", "ff", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+    -- elseif client.resolved_capabilities.document_range_formatting then
+    --     buf_set_keymap("n", "ff", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+    -- end
+    if client.server_capabilities.document_formatting then
         buf_set_keymap("n", "ff", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-    elseif client.resolved_capabilities.document_range_formatting then
+    elseif client.server_capabilities.document_range_formatting then
         buf_set_keymap("n", "ff", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
     end
 
