db3bcec4458fa35fccd3bbf6666f956a46df6b78
TMNT <trietpersonal@gmail.com>
2022-11-13 11:04:40 -0500

    dump

--- fish/.config/fish/functions/nvim.fish
+++ fish/.config/fish/functions/nvim.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-#function nvim --wraps='~/neovim/bin/nvim' --wraps=vim --description 'alias nvim=~/neovim/bin/nvim'
-#  ~/apps/nvim-linux64/bin/nvim  $argv;
-#end
+function nvim --wraps='~/apps/nvim.appimage' --wraps=vim --description '~/apps/nvim.appimage'
+  ~/apps/nvim.appimage  $argv;
+end
