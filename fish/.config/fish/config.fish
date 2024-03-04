86e56c52be9e31b876eaf09117f59c577680d947
trietmnj <trietn28@umd.edu>
2024-03-04 10:50:39 -0500

    fixed R package install segfault problem

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,6 +8,8 @@ else
         . "/home/tmnj/apps/miniconda3/etc/fish/conf.d/conda.fish"
     else
         set -x PATH "/home/tmnj/apps/miniconda3/bin" $PATH
+        set -x LANG en_US.UTF-8
+        set -x LC_ALL en_US.UTF-8
     end
 end
 # <<< conda initialize <<<
