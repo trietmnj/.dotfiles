9b120549876573cf36be4666db04069d88f966b8
tmnj <>
2025-09-01 17:15:10 -0400

    fix stow errors

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash 
-set -euo pipefail
-pushd "$DOTFILES" || exit 1
 for folder in stow-folders/*/; do
     pkg=$(basename "$folder")
     stow -d stow-folders -t ~ -D "$pkg"
     stow -d stow-folders -t ~ "$pkg"
 done
-popd
