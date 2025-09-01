056dc806780a3afbb4092496f8e457c780d5d9c0
tmnj <>
2025-09-01 17:19:58 -0400

    add nvim path for tmnj-desktop

--- stow-folders/fish/.config/fish/functions/nvim.fish
+++ stow-folders/fish/.config/fish/functions/nvim.fish
@@ -2,8 +2,10 @@
 function nvim
     set machine_name (hostname)
     if test $machine_name = "tTunesLaptop"
-	~/apps/squashfs-root/usr/bin/nvim $argv;
+        ~/apps/squashfs-root/usr/bin/nvim $argv;
+    else if test $machine_name = "tmnj-desktop"
+        /usr/local/bin/nvim $argv;
     else
-	~/apps/nvim-linux64/bin/nvim $argv;
+        ~/apps/nvim-linux64/bin/nvim $argv;
     end
 end
