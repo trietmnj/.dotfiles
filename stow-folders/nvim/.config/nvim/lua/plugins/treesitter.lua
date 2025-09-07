8a071445abc89ff54fe3540fd530ce4d6d195e51
tmnj <>
2025-09-07 12:51:09 -0400

    more cleaning up

--- stow-folders/nvim/.config/nvim/lua/plugins/treesitter.lua
+++ stow-folders/nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,77 +1,77 @@
 -- lua/plugins/treesitter.lua
 return {
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    branch = "master",
-    lazy = false,
-    event = { "BufReadPost", "BufNewFile" },
-    dependencies = { },
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "c",
-          "lua",
-          "vim",
-          "vimdoc",
-          "query",
-          "rust",
-          "go",
-          "typescript",
-          "python",
-
-          -- r requirements
-          "markdown", "markdown_inline", "r", "rnoweb", "yaml", "latex", "csv",
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        -- branch = "master",
+        lazy = false,
+        -- event = { "BufReadPost", "BufNewFile" },
+        dependencies = {},
+        opts = {
+            highlight = { enable = true },
+            indent = { enable = true },
+            context_commentstring = { enable = true, enable_autocmd = false },
+            incremental_selection = { enable = true },
         },
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {
+                    "c",
+                    "lua",
+                    "vim",
+                    "vimdoc",
+                    "query",
+                    "rust",
+                    "go",
+                    "typescript",
+                    "python",
 
-        highlight = { enable = true, disable = { "note" } },
-        indent = { enable = true },
-        context_commentstring = { enable = true, enable_autocmd = false },
-        incremental_selection = { enable = true },
-
-      })
-    end,
-  },
+                    -- r requirements
+                    "markdown", "markdown_inline", "r", "rnoweb", "yaml", "latex", "csv",
+                },
+            })
+        end,
+    },
 
     -- Rainbow Delimiters (after TS)
-  {
-    "HiPhish/rainbow-delimiters.nvim",
-    event = { "BufReadPost", "BufNewFile" },
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    config = function()
-      local rd = require("rainbow-delimiters")
-      vim.g.rainbow_delimiters = {
-        -- Global strategy works well for most; vimscript uses local for speed
-        strategy = {
-          [""]  = rd.strategy["global"],
-          vim   = rd.strategy["local"],
-        },
-        -- Default query name; you can override per filetype if needed
-        query = {
-          [""]    = "rainbow-delimiters",
-          -- latex = "rainbow-blocks",
-        },
-        -- Priority lets it win over theme highlights if needed
-        priority = {
-          [""] = 110,
-          lua  = 210,
-        },
-        -- Use the provided highlight groups (override these in your colors if you want custom colors)
-        highlight = {
-          "RainbowDelimiterRed",
-          "RainbowDelimiterYellow",
-          "RainbowDelimiterBlue",
-          "RainbowDelimiterOrange",
-          "RainbowDelimiterGreen",
-          "RainbowDelimiterViolet",
-          "RainbowDelimiterCyan",
-        },
-      }
-    end,
-  },
+    {
+        "HiPhish/rainbow-delimiters.nvim",
+        event = { "BufReadPost", "BufNewFile" },
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        config = function()
+            local rd = require("rainbow-delimiters")
+            vim.g.rainbow_delimiters = {
+                -- Global strategy works well for most; vimscript uses local for speed
+                strategy = {
+                    [""] = rd.strategy["global"],
+                    vim  = rd.strategy["local"],
+                },
+                -- Default query name; you can override per filetype if needed
+                query = {
+                    [""] = "rainbow-delimiters",
+                    -- latex = "rainbow-blocks",
+                },
+                -- Priority lets it win over theme highlights if needed
+                priority = {
+                    [""] = 110,
+                    lua  = 210,
+                },
+                -- Use the provided highlight groups (override these in your colors if you want custom colors)
+                highlight = {
+                    "RainbowDelimiterRed",
+                    "RainbowDelimiterYellow",
+                    "RainbowDelimiterBlue",
+                    "RainbowDelimiterOrange",
+                    "RainbowDelimiterGreen",
+                    "RainbowDelimiterViolet",
+                    "RainbowDelimiterCyan",
+                },
+            }
+        end,
+    },
 
-  -- Optional dev tool
-  { "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
+    -- Optional dev tool
+    { "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
 
 }
--- stow-folders/nvim/.config/nvim/lua/plugins/trouble.lua
+++ stow-folders/nvim/.config/nvim/lua/plugins/trouble.lua
@@ -1,21 +1,33 @@
 -- lua/plugins/trouble.lua
 return {
-  {
-    "folke/trouble.nvim",
-    cmd = "Trouble",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-      "nvim-treesitter/nvim-treesitter",
+    {
+        "folke/trouble.nvim",
+        cmd = "Trouble",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        init = function()
+            -- Disable Treesitter inside Trouble buffers to avoid the crash
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "trouble",
+                callback = function(args)
+                    pcall(vim.treesitter.stop, args.buf)
+                end,
+            })
+        end,
+        opts = {
+            multiline = false,
+            auto_preview = false,
+        },
+        keys = {
+            { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (workspace)" },
+            { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Diagnostics (buffer)" },
+            { "<leader>xs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols (document)" },
+            { "<leader>xq", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix" },
+            { "<leader>xl", "<cmd>Trouble loclist toggle<cr>", desc = "Location List" },
+            -- optional: LSP picker (defs/refs/impls/etc) in a right split
+            { "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "LSP (defs/refs/…)" },
+        },
     },
-    opts = {}, -- defaults are fine
-    keys = {
-      { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>",                         desc = "Diagnostics (workspace)" },
-      { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",            desc = "Diagnostics (buffer)" },
-      { "<leader>xs", "<cmd>Trouble symbols toggle focus=false<cr>",                 desc = "Symbols (document)" },
-      { "<leader>xq", "<cmd>Trouble qflist toggle<cr>",                              desc = "Quickfix" },
-      { "<leader>xl", "<cmd>Trouble loclist toggle<cr>",                             desc = "Location List" },
-      -- optional: LSP picker (defs/refs/impls/etc) in a right split
-      { "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",  desc = "LSP (defs/refs/…)" },
-    },
-  },
 }
